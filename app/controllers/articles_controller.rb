1850625870196a4bcfee5b5ffab1c9f0a9ea894d
Heinz Georg <heinzgeorg0303@gmail.com>
2015-09-15 06:34:47 -0700

    showing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,18 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(params.require(:article).permit(:title, :text))
+	  @article = Article.new(article_params)
+	 
+	  @article.save
+	  redirect_to @article
+	end
 
-		@article.save
-		redirect_to @article
-	end		
-end
+	def show
+	    @article = Article.find(params[:id])
+	end
+	 
+	private
+	  def article_params
+	    params.require(:article).permit(:title, :text)
+	  end
+ end
